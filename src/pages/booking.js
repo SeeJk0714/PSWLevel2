803c4d857375c4c74925fa71b5a225d670277243
jiekai <jiekai22s@forwardschool.edu.my>
2023-08-30 23:21:40 +0800

    u

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -83,6 +83,7 @@ export default function Booking() {
                                 onChange={(event) => {
                                     setRoom(event.target.value);
                                 }}
+                                disabled
                             />
                         </div>
                         <div className="mb-3">
